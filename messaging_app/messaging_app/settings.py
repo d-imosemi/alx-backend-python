d56e79d94d013d361308bcd727e72b829ce25965
Imosemi Ehioken Jeremiah <jay@Imosemis-MacBook-Pro.local>
2025-09-21 23:41:45 +0300

    new fix

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -72,11 +72,9 @@ TEMPLATES = [
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",  # require authentication by default
+    ],
 }
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
