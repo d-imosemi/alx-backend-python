259036656d7fbbd890796820865396432b7b2015
Imosemi Ehioken Jeremiah <jay@Imosemis-MacBook-Pro.local>
2025-09-28 15:28:17 +0300

    new code

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from .views import ConversationViewSet, MessageViewSet 
+from .views import ConversationViewSet, MessageViewSet  
 
 # Base router
 router = routers.DefaultRouter()
